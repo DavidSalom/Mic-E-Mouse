1fdbfab366d7e614535304f3c6fe8de92a7c0528
Mo Fakih <mhfakih@uci.edu>
2023-06-10 12:57:16 -0700

    Some local work

--- core/VCTK_dataloader.py
+++ core/VCTK_dataloader.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 class VCTK_Dataset(Dataset):
     def __init__(self, VCTK_root_path : str, **kwargs):
+        super(VCTK_Dataset, self).__init__()
         """
         VCTK_root_path: path to the VCTK dataset parent folder, which should contain wav48 and txt folders
         """
@@ -18,82 +19,30 @@ class VCTK_Dataset(Dataset):
         # Initialize the dataset
         self._init_ds()
         
-        if 'pad' in kwargs:
-            assert isinstance(kwargs['pad'], bool), "pad must be a boolean"
-            self.pad = kwargs['pad']
-        else:
-            self.pad = True
-        
-        if 'length_seconds' in kwargs:
-            assert isinstance(kwargs['length_seconds'], int), "length_seconds must be an integer"
-            self.length_seconds = kwargs['length_seconds']
-        else:
-            self.length_seconds = 5
-        
-        if 'normalize' in kwargs:
-            assert isinstance(kwargs['normalize'], bool), "normalize must be a boolean"
-            self.normalize = kwargs['normalize']
-        else:
-            self.normalize = True
+        self.resample_rate = kwargs.get('resample_rate', 8000)
             
-        if "resample" in kwargs:
-            assert isinstance(kwargs['resample'], bool), "resample must be a boolean"
-            self.resample = kwargs['resample']
-        else:
-            self.resample = True
-        
-        if "resample_rate" in kwargs:
-            assert isinstance(kwargs['resample_rate'], int), "resample_rate must be an integer"
-            self.resample_rate = kwargs['resample_rate']
-        else:
-            self.resample_rate = 16000
-            
-    def _normalize(self, waveform):
-        waveform = waveform - waveform.mean()
-        power = waveform.pow(2).mean()
-        waveform = waveform / power.sqrt()
-        return waveform
-        
     def _init_ds(self):
         # Get all the wav files paths
         self.wav_files_paths = glob.glob(os.path.join(self.VCTK_root_path, 'wav48', '*', '*.wav'))
-        self.txt_files_paths = [wav_file_path.replace('wav48', 'txt').replace('.wav', '.txt') for wav_file_path in self.wav_files_paths]
         
-        # Remove the files that don't have a corresponding txt file
-        self.wav_files_paths = [wav_file_path for wav_file_path, txt_file_path in zip(self.wav_files_paths, self.txt_files_paths) if os.path.exists(txt_file_path)]
-        self.txt_files_paths = [wav_file_path.replace('wav48', 'txt').replace('.wav', '.txt') for wav_file_path in self.wav_files_paths]
-        
-        # Get the speaker IDs
-        self.speaker_ids = [os.path.basename(os.path.dirname(wav_file)) for wav_file in self.wav_files_paths]
-        
-        # Get the utterance IDs
-        self.utterance_ids = [os.path.basename(wav_file).split('.')[0] for wav_file in self.wav_files_paths]
-        
-        # Load speaker info
-        self.speaker_info = self._process_info()
+        self.idx_to_wav_offset_dict = {}
+        self._build_idx_to_wav_dict()
     
-    def _process_info(self):
-        """
-        Load the speaker info from the speaker-info.txt file. Used in init.
-        """
-        speaker_info = {}
-        with open(os.path.join(self.VCTK_root_path, 'speaker-info.txt'), 'r') as f:
-            lines = f.readlines()
-            lines = [line.strip().split() for line in lines]
-            lines = lines[1:]
-            for line in lines:
-                id = line[0]
-                age= int(line[1])
-                gender= line[2]
-                accent= line[3]
-                regions= " ".join(line[4:])
-                speaker_info[id] = {
-                    'age': age,
-                    'gender': gender,
-                    'accent': accent,
-                    'regions': regions
-                }
-        return speaker_info
+    def _build_idx_to_wav_dict(self):
+        largest_idx = 0
+        print("Building the idx to wav offset dict...")
+        pbar = tqdm(total=len(self.wav_files_paths))
+        for wav_idx, wav_fn in enumerate(self.wav_files_paths):
+            wav, sr = self._load_audio(wav_fn)
+            number_of_one_second_chunks = wav.shape[1] // sr
+            # Check the last chunk if it is at least 0.8 seconds long
+            if wav.shape[1] % sr >= 0.8 * sr:
+                number_of_one_second_chunks += 1
+            for i in range(number_of_one_second_chunks):
+                self.idx_to_wav_offset_dict[largest_idx] = (wav_idx, i)
+                largest_idx += 1
+            pbar.update(1)
+        pbar.close()
     
     def _load_audio(self, wav_file_path : str):
         """
@@ -103,73 +52,30 @@ class VCTK_Dataset(Dataset):
         waveform, sample_rate = torchaudio.load(wav_file_path)
         return waveform, sample_rate
     
-    def _load_text(self, txt_file_path : str):
-        """
-        Load the text file from the txt file path. Used in __getitem__.
-        """
-        with open(txt_file_path, 'r') as f:
-            text = f.read().strip()
-        return text
-    
-    def _load_metadata(self, speaker_id):
-        try:
-            data = self.speaker_info[speaker_id]
-        except KeyError:
-            data = {
-                    'age': -1,
-                    'gender': "X",
-                    'accent': "X",
-                    'regions': "X"
-                }
-        return data
+    def _normalize(self, waveform):
+        waveform = waveform - waveform.mean()
+        power = waveform.pow(2).mean()
+        waveform = waveform / power.sqrt()
+        return waveform
     
     def __len__(self):
         return len(self.idx_to_wav_offset_dict)
 
     def __getitem__(self, idx):
-        wav_file_path = self.wav_files_paths[idx] # wav file path
-        txt_file_path = self.txt_files_paths[idx] # txt file path
-
-        speaker_id = self.speaker_ids[idx].replace('p', '') # speaker id, making sure to remove the 'p' at the beginning
-
-        # utterance_id = self.utterance_ids[idx]
+        idx = list(self.idx_to_wav_offset_dict.keys())[idx]
+        wav_idx, wav_offset = self.idx_to_wav_offset_dict[idx]
+        wav_file_path = self.wav_files_paths[wav_idx] # wav file path
         
         waveform, sample_rate = self._load_audio(wav_file_path)
-        
-        if self.resample:
-            waveform, sample_rate = self._resample(waveform, sample_rate, self.resample_rate)
-        
-        text = self._load_text(txt_file_path)
-        metadata = self._load_metadata(speaker_id)
-        
-        if self.normalize:
-            waveform = self._normalize(waveform)
-        
-        if self.pad:
-            waveform = self._pad(waveform, sample_rate, self.length_seconds)
-        
-        return waveform, sample_rate, text, metadata
-    
-    def get_speaker_info(self, speaker_id):
-        return self._load_metadata(speaker_id)
+        waveform = self._normalize(waveform)
+        waveform = waveform[:, wav_offset*sample_rate:(wav_offset+1)*sample_rate]
+        waveform, sample_rate = self._resample(waveform, sample_rate, self.resample_rate)
 
+        if waveform.shape[1] < sample_rate:
+            waveform = torch.nn.functional.pad(waveform, (0, sample_rate - waveform.shape[1]))    
+        
+        return waveform, sample_rate
+    
     def _resample(self, waveform: torch.Tensor, sample_rate : int, new_samplerate: int):
         resampled_waveform = torchaudio.transforms.Resample(sample_rate, new_samplerate)(waveform)
         return resampled_waveform, new_samplerate
-
-    def _pad(self, waveform : torch.Tensor, sample_rate : int, length_seconds:int=10):
-        """
-        Pad or clip the waveform to the desired length in seconds, and add an indicator in the second channel to mark which part is the original waveform.
-        waveform: torch.Tensor, shape=(1, n_samples)
-        sample_rate: int
-        length_seconds: int 
-        """
-        padded_waveform = torch.nn.functional.pad(waveform, (0, int(length_seconds*sample_rate)))
-        if padded_waveform.shape[1] > int(length_seconds*sample_rate):
-            padded_waveform = padded_waveform[:, :int(length_seconds*sample_rate)]
-            padded_waveform = self._normalize(padded_waveform)        
-        padded_indicator = torch.zeros_like(padded_waveform)
-        padded_indicator[:, :waveform.shape[1]] = 1
-        padded_indicator[:, waveform.shape[1]:] = 0
-        fused_waveform = torch.cat([padded_waveform, padded_indicator], dim=0)
-        return fused_waveform
---
+++ core/WaveUNet.py
@@ -0,0 +1,181 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .NetworkPrimitives import Residual
+from torch.nn import BatchNorm1d, Conv1d, MaxPool1d
+import math
+from einops import rearrange, repeat, reduce, einsum
+
+class ConvResidual(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size = 15):
+        super(ConvResidual, self).__init__()
+        self.net = nn.Sequential(
+            Residual(
+                nn.Sequential(
+                    nn.Conv1d(in_channels, in_channels, kernel_size=kernel_size, stride=1, padding='same'),
+                    BatchNorm1d(in_channels),
+                    nn.GELU(),
+                    nn.Conv1d(in_channels, in_channels, kernel_size=kernel_size, stride=1, padding='same')
+                )
+            ),
+            BatchNorm1d(in_channels),
+            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding='same'),
+            nn.GELU(),
+            BatchNorm1d(out_channels)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+
+class DownSample(nn.Module):
+    def __init__(self):
+        super(DownSample, self).__init__()
+        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.maxpool(x)
+        return x
+    
+class DownsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=15):
+        super(DownsampleBlock, self).__init__()
+        self.resconv = ConvResidual(in_channels, out_channels, kernel_size)
+        self.downsample = DownSample()
+
+    def forward(self, x):
+        x = self.resconv(x)
+        x = self.downsample(x)
+        return x
+
+
+class UpSampleLinear(nn.Module):
+    def __init__(self, n_channels):
+        super(UpSampleLinear, self).__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode='linear', align_corners=True)
+        self.conv = nn.Conv1d(n_channels, n_channels, kernel_size=5, stride=1, padding='same')
+        self.bn = BatchNorm1d(n_channels)
+        
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.conv(F.gelu(x))
+        x = self.bn(x)
+
+        return x
+    
+
+class CropConcat1d(nn.Module):
+    def __init__(self):
+        """
+        Crop and concatenate two 1d tensors of different sizes, assuming the first tensor is larger than the second one
+        """
+        super(CropConcat1d, self).__init__()
+
+    def forward(self, x1, x2):
+        center = x1.size(2) // 2
+        half = x2.size(2) // 2
+        x1 = x1[:, :, center-half:center+half]
+        x = torch.cat([x1, x2], dim=1)
+        return x
+        
+
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=1000):
+        super(SinusoidalPositionalEncoding, self).__init__()
+        self.register_buffer('pe', self._get_sinusoid_encoding_table(d_model, max_len))
+        
+    def _get_sinusoid_encoding_table(self, d_model, max_len):
+        """Sinusoid position encoding table"""
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position.float() * div_term.float())
+        pe[:, 1::2] = torch.cos(position.float() * div_term.float())
+        return pe.unsqueeze(0)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+class LearnedPositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=1000):
+        super(LearnedPositionalEncoding, self).__init__()
+        self.pe = nn.Embedding(max_len, d_model)
+        
+    def forward(self, x):
+        return self.pe(torch.arange(x.size(1)).to(x.device)).unsqueeze(0)
+
+class AttentionBlock(nn.Module):
+    def __init__(self, embed_dim, n_heads, dropout=0.1):
+        super(AttentionBlock, self).__init__()
+        self.n_heads = n_heads
+        self.embed_dim = embed_dim
+        self.scale = embed_dim ** -0.5
+        
+        self.w_q = nn.Linear(self.embed_dim, self.embed_dim * n_heads)
+        self.w_k = nn.Linear(self.embed_dim, self.embed_dim * n_heads)
+        self.w_v = nn.Linear(self.embed_dim, self.embed_dim * n_heads)
+        self.w_o = nn.Linear(self.embed_dim * n_heads, self.embed_dim)
+        
+    def forward(self, q, k, v, mask = None):
+        assert q.shape[-1] == self.embed_dim, 'Input dimension mismatch, expected %d, got %d' % (self.embed_dim, q.shape[-1])
+        assert k.shape[-1] == self.embed_dim, 'Input dimension mismatch, expected %d, got %d' % (self.embed_dim, k.shape[-1])
+        assert v.shape[-1] == self.embed_dim, 'Input dimension mismatch, expected %d, got %d' % (self.embed_dim, v.shape[-1])
+
+        q = self.w_q(q)
+        k = self.w_k(k)
+        v = self.w_v(v)       
+        
+        W = einsum("... i d, ... j d -> ... i j", q, k) * self.scale
+        
+        attn = self.w_o(attn)
+        
+        return attn
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_dim, n_heads, dropout = 0.1):
+        super(TransformerBlock, self).__init__()
+
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.attention = AttentionBlock(embed_dim, n_heads, dropout)
+        self.dropout1 = nn.Dropout(dropout)
+        
+        self.ln2 = nn.LayerNorm(embed_dim)
+        
+        self.fc1 = nn.Linear(embed_dim, embed_dim * n_heads)
+        self.fc2 = nn.Linear(embed_dim * n_heads, embed_dim)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, y):
+        att = self.ln1(x)
+        att = self.attention(att, y, y)
+        att = self.dropout1(att)
+        x = x + att
+        att = self.ln2(x)
+        att = self.fc1(att)
+        att = F.gelu(att)
+        att = self.fc2(att)
+        att = self.dropout2(att)
+        x = x + att
+        return x
+    
+# class SwinBlock(nn.Module):
+#     def __init__(self):
+#         super(SwinBlock, self).__init__()
+        
+
+# class SwinUNet1d(nn.Module):
+#     def __init__(self):
+#         super(SwinUNet1d, self).__init__()
+#         self.patch_size = 4
+#         self.embed_dim = 48
+#         self.n_heads = 4
+        
+        
+        
+        
+#     def forward(self, x):
+#         assert len(x.shape) == 3, 'Input dimension mismatch, expected 3, got %d' % (len(x.shape))
+#         B, C, T = x.shape
+#         assert C == 1, 'Input dimension mismatch, expected 1, got %d' % (C)
+        
+#         x = rearrange(x, 'b c (t r) -> b t (c r)', r = self.patch_size) # x.shape = (B, T//self.patch_size, self.patch_size)
+#         x = 
